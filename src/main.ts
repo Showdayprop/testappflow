67724b1f6d5e7cecbc33cc72dad5263883a4ba98
Chetan Bansal <chetan.bansal019@gmail.com>
2021-03-24 01:30:14 +0530

    main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
+import '@capacitor/core';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import '@capacitor/core';
 
 if (environment.production) {
   enableProdMode();
